8b345f42fa8a05002d6bf50d68950b28d94d4b58
Igor Machado <igor.machado@gmail.com>
2025-04-13 17:09:13 -0300

    improve macros for macos, but windows is broken... not priority now

--- include/modlog/modlog_macros.hpp
+++ include/modlog/modlog_macros.hpp
@@ -9,12 +9,13 @@
 // common macros for printing
 
 #define LOG(LEVEL) LOG_##LEVEL
-#define LOG_SILENT modlog::Log(modlog::SILENT)
-#define LOG_INFO modlog::Log(modlog::INFO)
-#define LOG_WARNING modlog::Log(modlog::WARNING)
-#define LOG_ERROR modlog::Log(modlog::ERROR)
-#define LOG_FATAL modlog::Log(modlog::FATAL)
-#define VLOG(VLEVEL) modlog::VLog(VLEVEL)
+#define LOG_SILENT modlog::Log(modlog::SILENT, std::source_location::current())
+#define LOG_INFO modlog::Log(modlog::INFO, std::source_location::current())
+#define LOG_WARNING \
+  modlog::Log(modlog::WARNING, std::source_location::current())
+#define LOG_ERROR modlog::Log(modlog::ERROR, std::source_location::current())
+#define LOG_FATAL modlog::Log(modlog::FATAL, std::source_location::current())
+#define VLOG(VLEVEL) modlog::VLog(VLEVEL, std::source_location::current())
 
 #ifndef NDEBUG
 #define DLOG(LEVEL) LOG(LEVEL)
