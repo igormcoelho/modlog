0b1f5eed0f4fd92c7d7565c0a92d2cac5b6654e5
Igor Machado <igor.machado@gmail.com>
2025-04-13 23:33:24 -0300

    add demo6 file

---
+++ demo/demo6.cpp
@@ -0,0 +1,20 @@
+// C++17 version
+
+#include <modlog/modlog.hpp>
+
+auto main() -> int {
+  using namespace modlog;
+  using modlog::LogLevel::Error;
+  using modlog::LogLevel::Info;
+  using modlog::LogLevel::Silent;
+
+  Log(Info) << "Hello World!";
+  Log(Error) << "Hello World! Again...";
+  Log() << "Hello World! (this is INFO)";
+  Log(Silent) << "Hello World! (does not appear...)";
+
+  VLog(0) << "Hello World! (this is INFO too)";
+  VLog(1) << "Hello World! (this does not appear...)";
+
+  return 0;
+}
